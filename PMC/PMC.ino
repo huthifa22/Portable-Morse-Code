48d3ae08f99bc2ba58df2a78c88bee38d0e8e7f2
huthifa <huthifa001@gmail.com>
2024-12-17 20:46:20 -0500

    Using ArduinoCloudIoT Instead

--- PMC/PMC.ino
+++ PMC/PMC.ino
@@ -1,27 +1,28 @@
-#define BLYNK_TEMPLATE_ID "TMPL24b5q4mCt"
-#define BLYNK_TEMPLATE_NAME "PMC"
-
-#include "config.h"  
 #include <WiFiNINA.h>
-#include <BlynkSimpleWiFiNINA.h>
-
-char ssid[] = WIFI_SSID;
-char pass[] = WIFI_PASS;
+#include "thingProperties.h" 
+#include "config.h" 
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
   Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, pass);
+  WiFi.begin(SECRET_SSID, SECRET_OPTIONAL_PASS); 
   while (WiFi.status() != WL_CONNECTED);
   Serial.println("Wi-Fi Connected!");
 
-  Serial.println("Connecting to Blynk...");
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  Serial.println("Connected to Blynk!");
+  initProperties();
+
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  Serial.println("Connected to Arduino IoT Cloud!");
+
 }
 
 void loop() {
-  Blynk.run();
+   ArduinoCloud.update();
+}
+
+void onTextMessageChange() {
+  Serial.print("Message received: ");
+  Serial.println(textMessage);
 }
